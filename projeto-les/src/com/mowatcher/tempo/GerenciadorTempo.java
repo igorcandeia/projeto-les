dc9299442055f31f10f139ddba4bf042809f4994
Eldandil <rafaeleldandil@gmail.com>
2014-06-03 19:36:50 -0300

    Progresso em GerenciadorDeTempo
    Fiz algum progresso em GerenciadorDeTempo aqui...
    Falta só um método pra completá-lo e aí poderemos testar e acoplar na
    GUI

--- projeto-les/src/com/mowatcher/tempo/GerenciadorTempo.java
+++ projeto-les/src/com/mowatcher/tempo/GerenciadorTempo.java
@@ -11,11 +11,11 @@ public class GerenciadorTempo {
 	// private List<String> atividades;
 	private Calendar calendario;
 	private List<TempoInvestido> tIs;
-	
+
 	public GerenciadorTempo() {
 		tIs = TempoInvestido.getAll();
 	}
-	
+
 	public List<String> getAtividades() {
 		List<String> atividades = new ArrayList<String>();
 		for (TempoInvestido t : tIs) {
@@ -23,44 +23,91 @@ public class GerenciadorTempo {
 		}
 		return atividades;
 	}
-	
+
 	public Calendar getCalendario() {
 		return calendario;
 	}
-	
+
 	public void setCalendario(Calendar calendario) {
 		this.calendario = calendario;
 	}
+
 	public GerenciadorTempo(Calendar calendario) {
 		super();
 		this.calendario = calendario;
 	}
-	public void adicionaTI(TempoInvestido tI){
+
+	public void adicionaTI(TempoInvestido tI) {
 		tIs.add(tI);
 		tI.save(); // salva no BD o tempo investido
 	}
-	public String[] getAtividadesMaisRecentes(){
-		//TODO
+
+	public String[] getAtividadesMaisRecentes() {
+		List<String> atividades = new ArrayList<String>();
+		for (TempoInvestido t : tIs) {
+			if ((t.getSemanaDoAno() == Calendar.WEEK_OF_YEAR)
+					&& (t.getAno() == Calendar.YEAR))
+				atividades.add(t.getAtividade());
+		}
+		String[] array = {};
+		return atividades.toArray(array);
+	}
+
+	public List<VisualizacaoRelatorio> getRelatorioSemanal(int semana) {
+		// TODO
 		return null;
 	}
-	public VisualizacaoRelatorio getRelatorioSemanal(int semana){
-		//TODO
-		return null;
+
+	public float[] getPercentualUso(int semana) {
+		List<TempoInvestido> tempos = getTemposSemana(semana);
+		float[] percentuais = { 0, 0 };
+		float total = 0;
+		for (TempoInvestido t : tempos) {
+			if (t.getTipo().equals(EnumTipo.LAZER)) {
+				percentuais[0] += t.getTempo();
+			} else if (t.getTipo().equals(EnumTipo.TRABALHO)) {
+				percentuais[1] += t.getTempo();
+			}
+		}
+		for (int i = 0; i <= 1; i++) {
+			total += percentuais[i];
+		}
+		for (int i = 0; i <= 1; i++) {
+			percentuais[i] /= total;
+		}
+		return percentuais;
+
 	}
-	public float[] getPercentualUso() {
-		//TODO
-		return null;
+
+	public float[] getPercentualPrioridade(int semana) {
+		List<TempoInvestido> tempos = getTemposSemana(semana);
+		float[] percentuais = { 0, 0, 0, 0, 0 };
+		float total = 0;
+		for (TempoInvestido t : tempos) {
+			if (t.getTipo().equals(EnumPrioridade.BAIXISSIMA)) {
+				percentuais[0] += t.getTempo();
+			} else if (t.getTipo().equals(EnumPrioridade.BAIXA)) {
+				percentuais[1] += t.getTempo();
+			} else if (t.getTipo().equals(EnumPrioridade.MEDIA)) {
+				percentuais[2] += t.getTempo();
+			} else if (t.getTipo().equals(EnumPrioridade.ALTA)) {
+				percentuais[3] += t.getTempo();
+			} else if (t.getTipo().equals(EnumPrioridade.ALTISSIMA)) {
+				percentuais[4] += t.getTempo();
+			}
+		}
+		for (int i = 0; i <= 4; i++) {
+			total += percentuais[i];
+		}
+		for (int i = 0; i <= 4; i++) {
+			percentuais[i] /= total;
+		}
+		return percentuais;
 	}
-	public float[] getPercentualPrioridade() {
-		//TODO
-		return null;
-	}
-	
+
 	/**
-	 * Retorna os Tempos Investidos de uma certa semana começando do domingo 
-	 * Ex:
-	 * semana=0 => tempos da semana atual
-	 * semana=1 => tempos da semana passada
+	 * Retorna os Tempos Investidos de uma certa semana comeï¿½ando do domingo Ex:
+	 * semana=0 => tempos da semana atual semana=1 => tempos da semana passada
 	 */
 	public List<TempoInvestido> getTemposSemana(int semana) {
 		return TempoInvestido.getTemposDaSemana(semana);
--- projeto-les/src/com/mowatcher/tempo/VisualizacaoRelatorio.java
+++ projeto-les/src/com/mowatcher/tempo/VisualizacaoRelatorio.java
@@ -1,6 +1,6 @@
 package com.mowatcher.tempo;
 
-public class VisualizacaoRelatorio implements Comparable<VisualizacaoRelatorio>{
+public class VisualizacaoRelatorio implements Comparable<VisualizacaoRelatorio> {
 	private String atividade;
 	private float tempoInvestido;
 	private EnumTipo tipo;
@@ -9,27 +9,35 @@ public class VisualizacaoRelatorio implements Comparable<VisualizacaoRelatorio>{
 	public String getAtividade() {
 		return atividade;
 	}
+
 	public void setAtividade(String atividade) {
 		this.atividade = atividade;
 	}
+
 	public float getTempoInvestido() {
 		return tempoInvestido;
 	}
+
 	public void setTempoInvestido(float tempoInvestido) {
 		this.tempoInvestido = tempoInvestido;
 	}
+
 	public EnumTipo getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(EnumTipo tipo) {
 		this.tipo = tipo;
 	}
+
 	public EnumPrioridade getPrioridade() {
 		return prioridade;
 	}
+
 	public void setPrioridade(EnumPrioridade prioridade) {
 		this.prioridade = prioridade;
 	}
+
 	public VisualizacaoRelatorio(String atividade, float tempoInvestido,
 			EnumTipo tipo, EnumPrioridade prioridade) {
 		super();
@@ -38,12 +46,12 @@ public class VisualizacaoRelatorio implements Comparable<VisualizacaoRelatorio>{
 		this.tipo = tipo;
 		this.prioridade = prioridade;
 	}
+
 	@Override
 	public int compareTo(VisualizacaoRelatorio outra) {
-		if (this.tempoInvestido > outra.getTempoInvestido()){
+		if (this.tempoInvestido > outra.getTempoInvestido()) {
 			return -1;
-		}
-		else if (this.tempoInvestido < outra.getTempoInvestido()){
+		} else if (this.tempoInvestido < outra.getTempoInvestido()) {
 			return 1;
 		}
 		return 0;
