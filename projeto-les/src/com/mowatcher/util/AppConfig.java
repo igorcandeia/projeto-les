ad2842ca80417cc169a29ea30e96e9686d3ef4d4
claudivanfilho <j.claudivan.filho@gmail.com>
2014-06-11 06:58:14 -0300

    Alteracao para BD-Local

--- projeto-les/src/com/mowatcher/util/AppConfig.java
+++ projeto-les/src/com/mowatcher/util/AppConfig.java
@@ -2,6 +2,6 @@ package com.mowatcher.util;
 
 public class AppConfig {
 	public static final String HOST_PORT = "http://ec2-54-187-243-43.us-west-2.compute.amazonaws.com:9090/";
-	public static final boolean BD_LOCAL = false;
-	public static final boolean BD_REMOTE = true;
+	public static final boolean BD_LOCAL = true;
+	public static final boolean BD_REMOTE = false;
 }
